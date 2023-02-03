1fb4da84a1c303c0b357184e381fad28f9b601ff
Raul <raul.lara.unam@gmail.com>
2023-02-03 14:06:55 -0600

    Se optimiza el navbar

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -4,74 +4,75 @@ import { Link, NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
 
-    const toggleMenu = () => {
-        document.body.classList.toggle("open");
-    };
-
     return (
         <>
-            <NavLink
-                className="navbar"
-                to="/"
-            >
-                <img className="navbar-logo" src="../../../assets/images/navbar/logo.png" />
-            </NavLink>
-            <button className="navbar-burger" onClick={toggleMenu}></button>
-            <nav className="menu">
-                <NavLink
-                    //className={({ isActive }) => `nav-item  nav-link ${isActive ? 'active' : ''}`}
-                    to="/restaurants"
-                    onClick={ toggleMenu }
 
-                >
-                    Home
-                </NavLink>
-
-                <NavLink
-                    //className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                    to="/search"
-                    onClick={ toggleMenu }
-                >
-                    Search Restaurants
-                </NavLink>
-            </nav>
-        </>
-        /*
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <Link 
-                    className="navbar-brand" 
-                    to="/"
-                >
-                    Melp
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav ms-auto ">
-                    
-                    <NavLink 
-                        className={ ({ isActive }) => `nav-item  nav-link ${ isActive ? 'active' :'' }` }
+            <nav className="navbar navbar-dark bg-dark fixed-top">
+                <div className="container-fluid">
+                    <NavLink
+                        //classNameName={({ isActive }) => `nav-item  nav-link ${isActive ? 'active' : ''}`}
                         to="/restaurants"
                     >
-                        Restaurants
+                        <img classNameName="navbar-logo" src="../../../assets/images/navbar/logo.png" />
                     </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+                            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li className="nav-item">
+                                    <NavLink 
+                                        className="nav-link active" 
+                                        aria-current="page"
+                                        to="/"
+                                    >
+                                        Home
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item">
+                                    <NavLink
+                                        className="nav-link active"
+                                        aria-current="page"
+                                        to="/search"
+                                    >
+                                        Link
+                                    </NavLink>
+                                </li>
+                                <li className="nav-item dropdown">
+                                    <NavLink 
+                                        className="nav-link dropdown-toggle" 
+                                        role="button" 
+                                        data-bs-toggle="dropdown" 
+                                        aria-expanded="false"
+                                        to="/"
+                                    >
+                                        Dropdown
+                                    </NavLink>
+                                    <ul className="dropdown-menu dropdown-menu-dark">
+                                        <li><NavLink className="dropdown-item" to="/">Action</NavLink></li>
+                                        <li><NavLink className="dropdown-item" to="/">Another action</NavLink></li>
+                                        <li>
+                                            <hr className="dropdown-divider" />
+                                        </li>
+                                        <li><NavLink className="dropdown-item" to="/">Something else here</NavLink></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <form className="d-flex mt-3" role="search">
 
-                    <NavLink 
-                        className={ ({ isActive }) => `nav-item nav-link ${ isActive ? 'active' :'' }` }
-                        to="/search"
-                    >
-                        Search Restaurants
-                    </NavLink>
+                                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                <button className="btn btn-success" type="submit">Search</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-        </nav>
-
-
-    */
+            </nav>
+       </>
 
     )
 }
