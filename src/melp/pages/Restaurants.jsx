91cc26c18d6abea00e9b21b203c26274dd816079
Raul <raul.lara.unam@gmail.com>
2023-02-03 18:16:05 -0600

    Actualizacion en lapagina de inicio, se actualizar el footer

--- src/melp/pages/Restaurants.jsx
+++ src/melp/pages/Restaurants.jsx
@@ -1,38 +1,72 @@
+import { Link, useNavigate } from 'react-router-dom';
 import { RestaurantList } from '../components/RestaurantList';
 
 
 export const Restaurants = () => {
 
-  
+  const navigate = useNavigate();
 
-    return (
-      <>
-          <h2 className="banner-main-page">Wellcome to Melp</h2>
-          
-          <div className="banner container animated tada">
-          <div className=" big-text animated tada">New Restaurants!</div>
-            <div>Discover now</div>
-            
-          </div>
-          
-            
-            <div className="container mb-3">
-              <span className="span-location">It looks like you're in Mexico City</span>
-                <hr />
-              <h3 className="h3-main-page">Available Now</h3>
-                
-              <RestaurantList state='Oaxaca' />
+  const onChange = () => {
 
+    navigate( '/search' );
 
-              <h3 className="h3-main-page">Recommended</h3>
-                
-                <RestaurantList state='Guerrero' />
-
-            </div>
-
-            
-          
-      </>
-    )
   }
-  
+
+  return (
+    <>
+      <h2 className="banner-main-page">Wellcome to Melp</h2>
+
+      <div className="banner container animated tada">
+        <div className=" big-text animated tada">New Restaurants!</div>
+        <div>Discover now</div>
+
+      </div>
+
+
+      <div className="container mb-3">
+        <span className="span-location">It looks like you're in Mexico City</span>
+        <hr />
+        <h3 className="h3-main-page banner-available-now">Available Now</h3>
+
+        <RestaurantList state='Oaxaca' />
+
+
+        <h3 className="h3-main-page banner-recommend">Recommended</h3>
+
+        <RestaurantList state='Veracruz' />
+
+        <h3 className="h3-main-page banner-recommend">Trending</h3>
+
+        <RestaurantList state='Chiapas' />
+
+        <h3 className="h3-main-page banner-recommend">Explore</h3>
+
+        <div className="explore-state">
+          <ul>
+            <li><Link className="explore-list">Puebla</Link></li>
+            <li><Link className="explore-list">Guerreo</Link></li>
+            <li><Link className="explore-list">Chiapas</Link></li>
+          </ul>
+          <ul>
+            <li><Link className="explore-list">Guanajuato</Link></li>
+            <li><Link className="explore-list">Tabasco</Link></li>
+            <li><Link className="explore-list">Sinaloa</Link></li>
+          </ul>
+          <ul>
+            <li><Link className="explore-list">Campeche</Link></li>
+            <li><Link className="explore-list">Durango</Link></li>
+            <li><Link className="explore-list">Quintana Roo</Link></li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div className="banner-bottom container animated tada">
+        <div className=" big-text animated tada">Enjoy the best flavor</div>       
+        <button className="btn btn-primary" onClick={ onChange }>Search a restaurant</button>
+
+      </div>
+
+    </>
+  )
+}
--- src/ui/components/Footer.jsx
+++ src/ui/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 export const Footer = () => {
@@ -6,15 +7,48 @@ export const Footer = () => {
 
     return (
         <>
-            <footer className="bg-dark text-center text-lg-start text-light">
-                
-                <div className="text-center p-3">
-                    {`© ${anioCopyright}`} Copyright: &nbsp;
-                    <a className="text-light" href="https://www.linkedin.com/in/raul-lara-hernandez/">
-                        Raul Lara
-                    </a>
+            <footer className=" text-center text-white">
+
+                <div className="container p-4 container-footer">
+
+
+                    <section className="mb-4">
+
+                        <Link className="img-social" role="button">
+                            <i className="fab fa-facebook-f">
+                                <img className="img-social" src="../../../assets/images/socialMedia/githubLogo.png" alt="" />
+                            </i>
+                        </Link>
+
+                        <Link  href="#!" role="button">
+                            <i className="fab fa-facebook-f">
+                                <img className="img-social" src="../../../assets/images/socialMedia/linkedinLogo.png" alt="" />
+                            </i>
+                        </Link>
+
+                    </section>
+
+                    <section className="mb-4">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                            distinctio earum repellat quaerat voluptatibus placeat nam,
+                            commodi optio pariatur est quia magnam eum harum corrupti dicta,
+                            aliquam sequi voluptate quas.
+                        </p>
+                    </section>
+
+
+
                 </div>
-                
+
+
+
+                <div className="text-center p-3 container container-copyright">
+                    © {anioCopyright} Copyright:&nbsp;
+                    <span className="text-white">Raul Lara</span>
+                </div>
+
+
             </footer>
         </>
     )
