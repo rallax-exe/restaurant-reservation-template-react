796325ed43c9b1d96c62815b9567cc3437901a63
Raul <raul.lara.unam@gmail.com>
2023-02-07 18:20:27 -0600

    Se agrega pagina de contacto

--- src/melp/pages/Contact.jsx
+++ src/melp/pages/Contact.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
 export const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <>
+            <h2 className="banner-page">Contact</h2>
+            <div className="container">
+                <h3>About me</h3>
+                <span>Hi, I'm Raul Lara a computer engineer, I developed this website using React and Bootstrap</span>
+                
+                <h5 className="h5-social-media">My social media:</h5>
+                <ul>                    
+                    <li><a href="https://github.com/rallax-exe" target="_blank">My Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/raul-lara-hernandez/" target="_blank">Follow me in LikedIn</a></li>
+                </ul>
+                
+            </div>
+        </>
+    )
 }
+
