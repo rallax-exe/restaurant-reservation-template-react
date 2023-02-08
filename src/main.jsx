84e28c76383ed40a137168ccdf581331a75e4cb6
Raul <raul.lara.unam@gmail.com>
2023-02-08 13:28:36 -0600

    Se agrega hash router para que en produccion las rutas nunca se salgan del path y evitar errores 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { MelpApp } from './MelpApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <MelpApp />
-    </BrowserRouter>
+    {/*<BrowserRouter>*/}
+    <HashRouter>
+      <MelpApp />
+    </HashRouter>
+    {/*</BrowserRouter>*/}
   </React.StrictMode>
 );
